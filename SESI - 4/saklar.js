61d15dd83c5351b2339042f7a6e72bd4cce89ebc
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-02-23 19:07:19 +0700

    update sudah berhasil menyala abangkuhhh

--- SESI - 4/saklar.js
+++ SESI - 4/saklar.js
@@ -1,22 +1,34 @@
 // func saklar off and onn
-function saklar(params) {
+function saklar() {
+    let toggel1 = document.getElementById("defaultToggle1")
+    let toggel2 = document.getElementById("defaultToggle2")
+    let toggel3 = document.getElementById("defaultToggle3")
 
-    // memanggil id lampu1
-    let lampu1 = document.querySelector("#lampu1");
-    let lampu2 = document.querySelector("#lampu2");
-    let lampu3 = document.querySelector("#lampu3");
-    
-    // jika parameter sama dengan string "on"
-    if (params == "on") {                
-        // maka lampu nyala
-        lampu1.src = "assets/on.png";
+    let lampu1 = document.getElementById("lampu1");
+    let lampu2 = document.getElementById("lampu2");
+    let lampu3 = document.getElementById("lampu3");
+
+    if(toggel1.checked){
+        lampu1.src = "assets/on.png"
+
+    }else{  
+        lampu1.src = "assets/off.png"
     }
-    // jika parameter sama dengan string "off"
-    if(params == "off") {
-        // maka lampu mati
-        lampu1.src = "assets/off.png";
+    if(toggel2.checked){
+        lampu2.src = "assets/on.png"
+
+    }else{  
+        lampu2.src = "assets/off.png"
     }
 
-    return lampu1;
+    if(toggel3.checked){
+        lampu3.src = "assets/on.png"
+
+    }else{  
+        lampu3.src = "assets/off.png"
+    }
+
+
+
 
 }
