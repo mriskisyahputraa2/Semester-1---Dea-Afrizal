70c40074736138e261cc232aa95d542c2b2d6cf2
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-03-19 00:02:52 +0700

    setup server

--- SESI-7-nodeJs/index.js
+++ SESI-7-nodeJs/index.js
@@ -1,10 +1,16 @@
 const http = require("http");
-const host = "localhost";
-const port = 3002;
+const host = "127.0.0.1"; // ini localhost
+const port = 3002; // ini port yang dijalankan
+
+// request adalah = data masuk dari luar
+// response adalah = data keluar dari sistem
 
 const server = http.createServer(function (request, response) {
-  response.end("Hello world");
+  response.statusCode = 203; // mengatur status code
+  response.end("Hello brorrrs");
 });
-server.listen(port, host, "", function () {
-  console.log("server menyala...");
+server.listen(port, host, function () {
+  console.log(`server menyala di ${host}:${port}`);
 });
+
+// video 42.29
